5708bc7177f69e9ca62db85f143a54177b8ff40e
YANINA <yanka.majzus@yandex.ru>
2022-08-06 12:08:24 +0300

    kotlinCalculatorHW1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    readLine()?.let {
+        try {
+            println(someLogic(it))
+        } catch (e: IllegalExpressionException) {
+            println(
+                "Error '${e.message} | ${it.substring(0, e.index)}${
+                    if (e.index < it.length)
+                        " >> ${it.substring(e.index, e.index + 1)} << ${it.substring(e.index)}"
+                    else
+                        "??"
+                }"
+            )
+        }
+    }
+}
